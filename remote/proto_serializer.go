8302a4b18b121647f71b42ae4bdd3299cef176c7
Üstün Ergenoglu <ustun@noice.com>
2022-09-05 08:46:26 +0300

    Improve protobuf serializer error messages

--- remote/proto_serializer.go
+++ remote/proto_serializer.go
@@ -1,11 +1,16 @@
 package remote
 
 import (
+	"errors"
 	"fmt"
 
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/reflect/protoregistry"
+)
+
+var (
+	ErrNotProtoMessage = errors.New("msg is not proto.Message")
 )
 
 type protoSerializer struct{}
@@ -23,7 +28,7 @@ func (p *protoSerializer) Serialize(msg interface{}) ([]byte, error) {
 
 		return bytes, nil
 	}
-	return nil, fmt.Errorf("msg must be proto.Message")
+	return nil, fmt.Errorf("%w: %T", ErrNotProtoMessage, msg)
 }
 
 func (p *protoSerializer) Deserialize(typeName string, bytes []byte) (interface{}, error) {
@@ -41,5 +46,5 @@ func (protoSerializer) GetTypeName(msg interface{}) (string, error) {
 
 		return string(typeName), nil
 	}
-	return "", fmt.Errorf("msg must be proto.Message")
+	return "", fmt.Errorf("%w: %T", ErrNotProtoMessage, msg)
 }
